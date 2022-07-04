423b1850e794d8ba07e892f45e1a03b450bfec09
Daniel Rojas Zúñiga <cld.rojasz@gmail.com>
2022-07-04 02:47:44 -0400

    Add projects section

---
+++ src/components/Project.svelte
@@ -0,0 +1,65 @@
+<script>
+  export let name, imgUrl, previewUrl, repoUrl, technologies;
+</script>
+
+<article class="project">
+  <img class="image" src={imgUrl} alt={name} />
+
+  <div class="details">
+    <div class="title">{name}</div>
+
+    <div class="links">
+      <a href={previewUrl} class="button">Preview</a>
+      <a href={repoUrl} class="button">Source Code</a>
+    </div>
+
+    <ul class="technologies">
+      {#each technologies as tech}
+        <li class="tech">{tech}</li>
+      {/each}
+    </ul>
+  </div>
+</article>
+
+<style>
+  .project {
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    border-radius: 10px;
+    border: solid 1px red;
+  }
+  .image {
+    width: 100%;
+    position: static;
+    top: 0;
+  }
+  .details {
+    display: flex;
+    flex-direction: column;
+    background: rgba(13, 33, 80, 0.483);
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    z-index: 2;
+  }
+  .title {
+    background-color: rgba(1, 84, 84, 0.428);
+    position: absolute;
+    bottom: 5rem;
+    display: flex;
+    width: 100%;
+    justify-content: center;
+  }
+  .links {
+    border: solid 1px green;
+    display: flex;
+    justify-content: space-around;
+  }
+  .technologies {
+    display: flex;
+  }
+  .tech {
+    padding: 4px;
+  }
+</style>
---
+++ src/pages/projects.svelte
@@ -0,0 +1,31 @@
+<script>
+  import Project from "../components/Project.svelte";
+</script>
+
+<section class="projects">
+  <h2 class="title">Projects</h2>
+  <Project
+    name="To-DO List"
+    imgUrl="src/assets/projects/todo.png"
+    previewUrl="https://cldrojas.github.io/todo"
+    repoUrl="github.com/cldrojas/todo"
+    technologies={[
+      "html",
+      "css",
+      "javascript",
+      "svelte",
+      "github-pages",
+      "travis-ci",
+    ]}
+  />
+</section>
+
+<style>
+  .projects {
+    min-height: 100vh;
+    padding: 8px 24px;
+  }
+  .title {
+    margin-bottom: 16px;
+  }
+</style>
