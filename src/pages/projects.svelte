a21efabe12589902a9c615bddebb4d7665c94f83
Daniel Rojas Zúñiga <cld.rojasz@gmail.com>
2022-07-07 23:09:26 -0400

    Fix assets import

--- src/pages/projects.svelte
+++ src/pages/projects.svelte
@@ -1,5 +1,12 @@
 <script>
   import Project from "../components/Project.svelte";
+
+  import todo from "../assets/projects/todo.png";
+  import todoLogo from "../assets/projects/todo_logo.png";
+  import vero from "../assets/projects/vero.png";
+  import veroLogo from "../assets/projects/vero_logo.png";
+  import rick from "../assets/projects/rick.png";
+  import rickLogo from "../assets/projects/rick_logo.png";
 </script>
 
 <section class="projects">
@@ -8,8 +15,8 @@
     <Project
       name="To-Do List"
       description="This is my first project using svelte, it's a frontend mentor challenge"
-      imgUrl="src/assets/projects/todo.png"
-      logoUrl="src/assets/projects/todo_logo.png"
+      imgUrl={todo}
+      logoUrl={todoLogo}
       previewUrl="https://cldrojas.github.io/todo"
       repoUrl="https://github.com/cldrojas/todo"
       technologies={[
@@ -25,8 +32,8 @@
     <Project
       name="Verónica"
       description="Landing page for promoting my girlfriend's work"
-      imgUrl="src/assets/projects/vero.png"
-      logoUrl="src/assets/projects/vero_logo.png"
+      imgUrl={vero}
+      logoUrl={veroLogo}
       previewUrl="https://cldrojas.github.io/veronica"
       repoUrl="https://github.com/cldrojas/veronica"
       technologies={[
@@ -42,8 +49,8 @@
     <Project
       name="Rick & Morty"
       description="A single page application that shows Rick & Morty characters"
-      imgUrl="src/assets/projects/rick.png"
-      logoUrl="src/assets/projects/rick_logo.png"
+      imgUrl={rick}
+      logoUrl={rickLogo}
       previewUrl="https://cldrojas.github.io/spa-node/"
       repoUrl="https://github.com/cldrojas/spa-node"
       technologies={[
