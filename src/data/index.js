9ce71b25cb7cda4b16571288afceac6db4b6a03e
cldrojas <cld.rojasz@gmail.com>
2022-10-09 02:32:51 -0300

    Extract projects list into a new file

---
+++ src/data/index.js
@@ -0,0 +1,81 @@
+import todo from "../assets/projects/todo.png";
+import todoLogo from "../assets/projects/todo_logo.png";
+import vero from "../assets/projects/vero.png";
+import veroLogo from "../assets/projects/vero_logo.png";
+import rick from "../assets/projects/rick.png";
+import rickLogo from "../assets/projects/rick_logo.png";
+
+const projects = [
+  
+  {
+    "name":"React todo",
+    "description":"React intro course",
+    "imgUrl":todo,
+    "logoUrl":todoLogo,
+    "previewUrl":"https://cldrojas.github.io/react-todo",
+    "repoUrl":"https://github.com/cldrojas/react-todo",
+    "technologies":[
+      "html",
+      "css",
+      "javascript",
+      "svelte",
+      "webpack",
+      "travis-ci",
+      "github-pages",
+    ]
+  },
+  {
+    "name":"Verónica Crespo",
+    "description":"She's my girlfriend! ❤",
+    "imgUrl":vero,
+    "logoUrl":veroLogo,
+    "previewUrl":"https://cldrojas.github.io/todo",
+    "repoUrl":"https://github.com/cldrojas/todo",
+    "technologies":[
+      "html",
+      "css",
+      "javascript",
+      "svelte",
+      "webpack",
+      "travis-ci",
+      "github-pages",
+    ]
+  },
+  {
+    "name":"To-Do List",
+    "description":"Frontend mentor challenge",
+    "imgUrl":todo,
+    "logoUrl":todoLogo,
+    "previewUrl":"https://cldrojas.github.io/todo",
+    "repoUrl":"https://github.com/cldrojas/todo",
+    "technologies":[
+      "html",
+      "css",
+      "javascript",
+      "svelte",
+      "webpack",
+      "travis-ci",
+      "github-pages",
+    ]
+  },
+  
+  {
+    "name": "Rick & Morty",
+    "description": "Search for Rick & Morty characters",
+    "imgUrl":rick,
+    "logoUrl":rickLogo,
+    "previewUrl": "https://cldrojas.github.io/spa-node/",
+    "repoUrl": "https://github.com/cldrojas/spa-node",
+    "technologies": [
+      "html",
+      "css",
+      "javascript",
+      "node",
+      "webpack",
+      "travis-ci",
+      "github-pages"
+    ]
+  },
+]
+
+export default projects
--- src/pages/projects.svelte
+++ src/pages/projects.svelte
@@ -1,84 +1,29 @@
 <script>
+  import projects from "../data";
   import Project from "../components/Project.svelte";
-
-  import todo from "../assets/projects/todo.png";
-  import todoLogo from "../assets/projects/todo_logo.png";
-  import vero from "../assets/projects/vero.png";
-  import veroLogo from "../assets/projects/vero_logo.png";
-  import rick from "../assets/projects/rick.png";
-  import rickLogo from "../assets/projects/rick_logo.png";
 </script>
 
-<section class="projects">
-  <h2 class="title">Projects</h2>
-  <div class="projects-list">
-    <Project
-      name="To-Do List"
-      description="This is my first project using svelte, it's a frontend mentor challenge"
-      imgUrl={todo}
-      logoUrl={todoLogo}
-      previewUrl="https://cldrojas.github.io/todo"
-      repoUrl="https://github.com/cldrojas/todo"
-      technologies={[
-        "html",
-        "css",
-        "javascript",
-        "svelte",
-        "webpack",
-        "travis-ci",
-        "github-pages",
-      ]}
-    />
-    <Project
-      name="Verónica"
-      description="Landing page for promoting my girlfriend's work"
-      imgUrl={vero}
-      logoUrl={veroLogo}
-      previewUrl="https://cldrojas.github.io/veronica"
-      repoUrl="https://github.com/cldrojas/veronica"
-      technologies={[
-        "html",
-        "css",
-        "javascript",
-        "svelte",
-        "webpack",
-        "travis-ci",
-        "github-pages",
-      ]}
-    />
-    <Project
-      name="Rick & Morty"
-      description="A single page application that shows Rick & Morty characters"
-      imgUrl={rick}
-      logoUrl={rickLogo}
-      previewUrl="https://cldrojas.github.io/spa-node/"
-      repoUrl="https://github.com/cldrojas/spa-node"
-      technologies={[
-        "html",
-        "css",
-        "javascript",
-        "node",
-        "webpack",
-        "travis-ci",
-        "github-pages",
-      ]}
-    />
+<section class="projects" id="Projects">
+  <div class="page">
+    <h2 class="title">Projects</h2>
+    <div class="projects-list">
+      {#each projects as project}
+        <Project {...project} />
+      {/each}
+    </div>
   </div>
 </section>
 
 <style>
   .projects {
-    width: 100%;
-    min-height: 100vh;
-    padding: 8px 24px;
-  }
-  .title {
-    margin-bottom: 16px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
   .projects-list {
     display: grid;
     justify-items: center;
     grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-    gap: 16px;
+    gap: 1.5rem;
   }
 </style>
